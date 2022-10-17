07f41a0a71fdd2b85c0dc0a18103ad04b7b465f7
kaioportela <kaioportela@au.ufc.br>
2022-10-17 15:14:16 -0300

    adicionando pagina de compra

--- code.js
+++ code.js
@@ -1,38 +1,35 @@
 function criardias(){
-    for(let i = 1 ; i <= 31 ; i++){
-        $('.dias_date').append($('<option>', {
-            value: i,
-            text: i
-        }));
-    }
+  for(let i = 1 ; i <= 31 ; i++){
+      $('.dias_date').append($('<option>', {
+          value: i,
+          text: i
+      }));
+  }
 }
 
 
-
 document.querySelector(".mes1").addEventListener("change", () => {
-  document.querySelector(".dias_date").addEventListener("change",() => {
-    if(document.querySelector(".mes1").value == 2 && document.querySelector(".dias_date").value == 29){
-      $(".ano_date").empty()
-      criaranobi()
-    }else {
-      $(".ano_date").empty()
-      criarano()
-    }
-  })
+document.querySelector(".dias_date").addEventListener("change",() => {
+  if(document.querySelector(".mes1").value == 2 && document.querySelector(".dias_date").value == 29){
+    $(".ano_date").empty()
+    criaranobi() 
+  }else {
+    $(".ano_date").empty()
+    criarano()
+  }
+})
 })
 criardias()
 
 
-
 function criarano(){
-    for(let i = 1950 ; i <= 2004 ; i++){
-        $('.anos_date').append($('<option>', {
-            value: i,
-            text: i
-        }));
-    }
+  for(let i = 1950 ; i <= 2004 ; i++){
+      $('.anos_date').append($('<option>', {
+          value: i,
+          text: i
+      }));
+  }
 }criarano()
-
 
 
 // function criaranobi(){
@@ -50,7 +47,6 @@ function criarano(){
 
 
 
-
 // $(function(){
 //     $('.sim').click(function(){
 //       if ($(this).is(':checked'))
@@ -59,7 +55,6 @@ function criarano(){
 //       }
 //     });
 //   });
-
 
 
 
@@ -73,7 +68,7 @@ function criarano(){
 //             event.preventDefault()
 //             event.stopPropagation()
 //           }
-          
+        
 //           form.classList.add('was-validated')
 //         }, false)
 //       })
