02e2b41d9a4615394a35528e49081efb899c384f
kaioportela <kaioportela@au.ufc.br>
2022-11-24 13:48:48 -0300

    add rotas

--- backend/vite-project/src/router/index.ts
+++ backend/vite-project/src/router/index.ts
@@ -1,44 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import LoginView from "../views/LoginView.vue";
-import UserRegister from "../views/UserRegister.vue";
-import HomeForm from "../components/HomeForm.vue";
-import UserDescription from "../components/UserDescription.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
     {
-      path: "/",
+      path: "/home",
+      name: "home",
       component: HomeView,
-      children: [
-        {
-          path: "/home",
-          name: "home",
-          component: HomeForm,
-        },
-        {
-          path: "/profile",
-          name: "profile",
-          component: UserDescription,
-        },
-      ],
     },
+    {
+      path: "/alugar",
+      name: "alugar",
+      component: HomeView,
+    },
+
     {
       path: "/login",
       name: "login",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: LoginView,
+      component: HomeView,
     },
     {
-      path: "/userRegister",
-      name: "userRegister",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: UserRegister,
+      path: "/compra",
+      name: "compra",
+      component: HomeView,
+    },
+    {
+      path: "/anunciar",
+      name: "anunciar",
+      component:HomeView,
+    },
+    {
+      path: "/reserva",
+      name: "reserva",
+      component: HomeView,
     },
   ],
 });
